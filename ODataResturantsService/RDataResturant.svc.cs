ddb762ec8857161a062df34757fa23313ec514e9
Sameh Nawasra <samehnawasra@hotmail.com>
2017-02-20 13:12:12 +0200

    wcf service

---
+++ ODataResturantsService/RDataResturant.svc.cs
@@ -0,0 +1,34 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace ODataResturantsService
+{
+    public class RDataResturant : DataService<devSource12BEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+
+            // Give readonly access to all of the entities
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+          
+            // Pagesize will change the max number of rows returned
+            config.SetEntitySetPageSize("*", 10);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
